fcdf07da77a1ccfbdbb851f23807456347b9a9fb
Rohan Khanna <rohankhanna190@gmail.com>
2022-11-14 11:58:38 +0530

    Added stored procedure

--- stored_procedure.sql
+++ stored_procedure.sql
@@ -1,10 +1,96 @@
-CREATE OR REPLACE add_passenger(train_id INTEGER, dated DATE, coach_type VARCHAR, seats_needed INTEGER, passenger_name VARCHAR, )
+CREATE OR REPLACE add_train(train_id INTEGER, dated DATE, ac_coaches INTEGER, sl_coaches INTEGER)
     AS
     $$
     DECLARE
-        
+        INTEGER ac_seats;
+        INTEGER sl_seats;
+        INTEGER cc;
+        INTEGER ss
+        VARCHAR in_query;
     BEGIN
+        SET ac_seats = ac_coaches * 18 ;
+        SET sl_seats = sl_coaches * 24 ;
+        SET cc = 0 ;
+        SET ss = 0 ;
+        in_query = CONCAT("INSERT INTO trains(train_id, total_ac_seats, total_sl_seats, total_ac_coach, total_sl_coach) values (", train_id , "," , ac_seats, "," , sl_seats, ",", ac_coaches, ",", sl_coaches);
+        execute (in_query);
+    END
+    $$
 
+
+LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE book_ticket(train_id INTEGER, dated DATE, coach_type VARCHAR, passenger_name VARCHAR, pnr INTEGER, total_AC_coach INTEGER, total_SL_coach INTEGER)
+    AS
+    $$
+    DECLARE
+        current_coach INTEGER;
+        table_name VARCHAR;
+        current_coach_seats INTEGER;
+        query_for_seats VARCHAR;
+        query_avail VARCHAR;
+        avail INTEGER;
+        total_coaches INTEGER;
+        current_seats_filled INTEGER;
+        query_for_btype VARCHAR;
+        current_berth INTEGER;
+        total_berth INTEGER;
+        alloted_berth_id INTEGER;
+        alloted_berth_type VARCHAR;
+        insert_query VARCHAR;
+        insert_query2 VARCHAR;
+    BEGIN
+        table_name = CONCAT("for", coach_type);
+        IF (coach_type === 'AC') 
+            BEGIN
+                SET total_coaches = total_AC_coach ;
+                SET total_berth = 18;
+            END 
+        ELSE BEGIN
+                SET total_coaches = total_SL_coach ;
+                SET total_berth = 24;
+        END
+        SET current_coach_seats = 1;
+        LOOP
+            query_for_seats = CONCAT('SELECT COUNT(*) FROM bookings WHERE train_id = ', train_id , ' AND dated = ', dated, ' AND coach_id = ', current_coach);
+            EXIT WHEN current_coach > total_coaches;
+            
+            EXECUTE query_for_seats INTO current_seats_filled;
+            IF (coach_type === 'AC' AND current_seats_filled === 18)
+                current_coach = current_coach + 1;
+                CONTINUE;
+            IF (coach_type === 'SL' AND current_seats_filled === 24)
+                current_coach = current_coach + 1;
+                CONTINUE;
+            END
+                -- Now alloting berth_id
+                -- Now we know that current_coach has an available seat, so we select that birth_id
+                SET current_berth = 1 ;
+                LOOP
+                    EXIT WHEN current_berth > total_berth;
+                        -- Now check if current_coach and current_berth already has an entry in bookings table
+                        query_avail = CONCAT('SELECT COUNT(*) FROM bookings WHERE train_id = ', train_id , ' AND dated = ', dated, ' AND coach_id = ', current_coach, ' AND berth_no = ', current_berth);
+                        EXECUTE query_avail INTO avail;
+                        if(avail <> 0) 
+                        BEGIN
+                            current_berth = current_berth + 1 ;
+                            CONTINUE;
+                        END
+                        -- Here simply allot this 
+                        alloted_berth_id = current_berth ;
+                        query_for_type = CONCAT('SELECT berth_type FROM ', table_name, ' WHERE berth_id = ', alloted_berth_id);
+                        EXECUTE query_for_type INTO alloted_berth_type;
+                        insert_query = CONCAT('INSERT INTO bookings( train_id, dated, coach_id, coach_type, berth_no, berth_type, pnr) VALUES(', train_id, ',', dated, ',' ,coach_type,',' , alloted_berth_id, ',' ,alloted_berth_type, ', ', pnr,  ')');
+                        EXECUTE insert_query ;
+                        insert_query2 =  CONCAT('INSERT INTO passengers (pnr, pname) VALUES(', pnr, ',' ,passenger_name , ')');
+                        EXECUTE insert_query2 ;
+                    EXIT;
+                END LOOP
+            EXIT;
+        END LOOP
     END
     $$
 LANGUAGE plpgsql;
+
--- tables.sql
+++ tables.sql
@@ -1,42 +1,81 @@
-CREATE TABLE hasAC (
-    acoach_id SERIAL NOT NULL, 
-    aavail_seats INTEGER NOT NULL,
-    aberth_id INTEGER NOT NULL,
-    adated DATE NOT NULL,
-    atrain_id INTEGER
+CREATE TABLE trains(
+    train_id INTEGER NOT NULL,
+    dated DATE NOT NULL,
+    total_ac_seats INTEGER NOT NULL,
+    total_sl_seats INTEGER NOT NULL,
+    total_ac_coach INTEGER NOT NULL,
+    total_sl_coach INTEGER NOT NULL,
 );
 
-CREATE TABLE hasSL (
-    scoach_id SERIAL NOT NULL, 
-    savail_seats INTEGER NOT NULL,
-    sberth_id INTEGER NOT NULL,
-    sdated DATE NOT NULL,
-    strain_id INTEGER NOT NULL 
+CREATE TABLE bookings(
+    train_id INTEGER NOT NULL,
+    dated DATE NOT NULL,
+    coach_id INTEGER,
+    coach_type VARCHAR NOT NULL,
+    berth_no INTEGER NOT NULL,
+    berth_type VARCHAR NOT NULL,
+    pnr INTEGER NOT NULL,
+    FOREIGN KEY train_id REFERENCES trains(train_id);
+    FOREIGN KEY dated REFERENCES trains(dated);
+    FOREIGN KEY pnr REFERENCES passengers(pnr);
 );
 
-CREATE TABLE acdesc(
-    aberth_id SERIAL PRIMARY KEY ,
-    aberth_type VARCHAR(10) PRIMARY KEY NOT NULL,
-    aberth_id REFERENCES hasAC(aberth_id)
+CREATE TABLE forAC(
+    berth_id INTEGER ,
+    berth_type VARCHAR NOT NULL
 );
 
-CREATE TABLE sleeperdesc(
-    sberth_id INTEGER PRIMARY KEY ,
-    sberth_type VARCHAR(10) NOT NULL,
-    sberth_id REFERENCES hasSL(sberth_id)
+CREATE TABLE forSL(
+    berth_id INTEGER NOT NULL,
+    berth_type VARCHAR NOT NULL
 );
 
-CREATE TABLE passenger(
-    pid SERIAL NOT NULL,
-    pname VARCHRA(50) NOT NULL,
-    pcoach_id INTEGER NOT NULL,
-    pberth_id INTEGER NOT NULL,
-    pberth_type VARCHAR(10) NOT NULL,
-    pcoach_type CHAR NOT NULL,
-    PRIMARY KEY(pid)
+CREATE TABLE passengers(
+    pnr INTEGER NOT NULL,
+    pname VARCHAR NOT NULL,
 );
 
-CREATE TABLE ticket(
-    ticket_id SERIAL NOT NULL,
-    pid INTEGER NOT NULL
-);
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (1,'LB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (2,'LB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (3,'UB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (4,'UB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (5,'SL');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (6,'SU');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (7,'LB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (8,'LB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (9,'UB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (10,'UB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (11,'SL');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (12,'SU');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (13,'LB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (14,'LB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (15,'UB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (16,'UB');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (17,'SL');
+INSERT INTO forAC(aberth_id, aberth_type) VALUES (18,'SU');
+
+
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (1,'LB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (2,'MB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (3,'UB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (4,'LB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (5,'MB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (6,'UB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (7,'SL');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (8,'SU');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (9,'LB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (10,'MB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (11,'UB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (12,'LB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (13,'MB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (14,'UB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (15,'SL');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (16,'SU');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (17,'LB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (18,'MB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (19,'UB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (20,'LB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (21,'MB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (22,'UB');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (23,'SL');
+INSERT INTO forSL(sberth_id, sberth_type) VALUES (24,'SU');
