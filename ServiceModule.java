fb69b43ab5b1870d9fb66c8e733c5f17b8f44a60
raghavptdr18 <88372112+raghavptdr18@users.noreply.github.com>
2022-11-15 13:18:48 +0530

    Add files via upload

---
+++ ServiceModule.java
@@ -0,0 +1,122 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class QueryRunner implements Runnable
+{
+    //  Declare socket for client access
+    protected Socket socketConnection;
+
+    public QueryRunner(Socket clientSocket)
+    {
+        this.socketConnection =  clientSocket;
+    }
+
+
+    public void run()
+    {
+      try
+        {
+            //  Reading data from client
+            InputStreamReader inputStream = new InputStreamReader(socketConnection
+                                                                  .getInputStream()) ;
+            BufferedReader bufferedInput = new BufferedReader(inputStream) ;
+            OutputStreamWriter outputStream = new OutputStreamWriter(socketConnection
+                                                                     .getOutputStream()) ;
+            BufferedWriter bufferedOutput = new BufferedWriter(outputStream) ;
+            PrintWriter printWriter = new PrintWriter(bufferedOutput, true) ;
+            String clientCommand = "" ;
+            String responseQuery = "" ;
+            // Read client query from the socket endpoint
+            clientCommand = bufferedInput.readLine(); 
+            while( ! clientCommand.equals("#"))
+            {
+                
+                System.out.println("Recieved data <" + clientCommand + "> from client : " 
+                                    + socketConnection.getRemoteSocketAddress().toString());
+
+                /*******************************************
+                         Your DB code goes here
+                ********************************************/
+                
+                // Dummy response send to client
+                responseQuery = "******* Dummy result ******";      
+                //  Sending data back to the client
+                printWriter.println(responseQuery);
+                // Read next client query
+                clientCommand = bufferedInput.readLine(); 
+            }
+            inputStream.close();
+            bufferedInput.close();
+            outputStream.close();
+            bufferedOutput.close();
+            printWriter.close();
+            socketConnection.close();
+        }
+        catch(IOException e)
+        {
+            return;
+        }
+    }
+}
+
+/**
+ * Main Class to controll the program flow
+ */
+public class ServiceModule 
+{
+    private static final Connection NULL = null;
+    // Server listens to port
+    static int serverPort = 7008;
+    // Max no of parallel requests the server can process
+    static int numServerCores = 5 ;         
+    //------------ Main----------------------
+    public static void main(String[] args) throws IOException 
+    {    
+
+        // connecting with JDBC 
+
+        Connection connection = NULL;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("")
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+        // Creating a thread pool
+        ExecutorService executorService = Executors.newFixedThreadPool(numServerCores);
+        
+        try (//Creating a server socket to listen for clients
+        ServerSocket serverSocket = new ServerSocket(serverPort)) {
+            Socket socketConnection = null;
+            
+            // Always-ON server
+            while(true)
+            {
+                System.out.println("Listening port : " + serverPort 
+                                    + "\nWaiting for clients...");
+                socketConnection = serverSocket.accept();   // Accept a connection from a client
+                System.out.println("Accepted client :" 
+                                    + socketConnection.getRemoteSocketAddress().toString() 
+                                    + "\n");
+                //  Create a runnable task
+                Runnable runnableTask = new QueryRunner(socketConnection);
+                //  Submit task for execution   
+                executorService.submit(runnableTask);   
+            }
+        }
+    }
+}
+
